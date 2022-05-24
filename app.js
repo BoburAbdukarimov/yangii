7f3daf90b6757f4d62110cb87c48b6051730935e
Boburiy <87008144+bobur2003@users.noreply.github.com>
2022-05-24 13:09:06 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var options = {
+    strings: ['student', 'Web developer', 'web designer'],
+    typeSpeed: 70,
+    startDelay: 500,
+    backSpeed: 40,
+    backDelay: 1000,
+    loop: true,
+    loopCount: Infinity
+};
+let typed = new Typed('.element2', options);
